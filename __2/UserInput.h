92d68c56b8f532e9784863cca4bce2f84b265b29
CookOS1803 <usovich5321@gmail.com>
2022-04-05 17:10:52 +0300

    input pod zadanie

--- __2/UserInput.h
+++ __2/UserInput.h
@@ -8,19 +8,14 @@
         while (true) \
         { \
             command; \
-            std::string temp; \
-            std::getline(std::cin, temp); \
-            try \
-            { \
-                value = std::stoi(temp); \
-            } \
-            catch (...) \
-            { \
-                continue; \
-            } \
-            break; \
+            std::cin >> value; \
+            if (std::cin.good()) \
+                break; \
+            std::cin.clear(); \
+            std::cin.ignore(99999, '\n'); \
         } \
     } \
-    while (!(condition))
+    while (!(condition)); \
+    getc(stdin)
 
 #define INPUT(command, value) INPUT_CONDITION(command, value, true)
